126c332699b19e1736490e3be4e849b821be2efc
Nate Otto <ottonomy@gmail.com>
2020-09-23 16:59:06 -0700

    Revert "Bugfix/bp 3305 rebake assertions"

--- apps/mainsite/mixins.py
+++ apps/mainsite/mixins.py
@@ -8,8 +8,9 @@ from django.db import models
 from resizeimage.resizeimage import resize_contain
 
 from defusedxml.cElementTree import parse as safe_parse
+from hashlib import sha256
 
-from mainsite.utils import verify_svg, scrubSvgElementTree, hash_for_image
+from mainsite.utils import verify_svg, scrubSvgElementTree
 
 
 def _decompression_bomb_check(image, max_pixels=Image.MAX_IMAGE_PIXELS):
@@ -27,19 +28,32 @@ class HashUploadedImage(models.Model):
     def save(self, *args, **kwargs):
         original_hash = self.image_hash
         pending_hash = self.hash_for_image_if_open()
-        result = super(HashUploadedImage, self).save(*args, **kwargs)
-
         if pending_hash is not None and pending_hash != self.image_hash:
             self.image_hash = pending_hash
 
             if self.pk:
                 self.schedule_image_update_task()
 
-        return result
+        return super(HashUploadedImage, self).save(*args, **kwargs)
+
+    def hash_for_image(self):
+        # from https://nitratine.net/blog/post/how-to-hash-files-in-python/
+        try:
+            block_size = 65536
+            file_hash = sha256()
+            image_data = self.image
+            file_buffer = image_data.read(block_size)
+            while len(file_buffer) > 0:
+                file_hash.update(file_buffer)
+                file_buffer = image_data.read(block_size)
+            image_data.seek(0)
+            return file_hash.hexdigest()
+        except:
+            return ''
 
     def hash_for_image_if_open(self):
         if self.image and not self.image.closed:
-            return hash_for_image(self.image)
+            return self.hash_for_image()
         return None
 
     def schedule_image_update_task(self):
